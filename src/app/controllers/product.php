260d39bebd4656640090cd3f54a2c2015a313e00
nhsmai04 <mr.nguyenhandsome04@gmail.com>
2024-12-10 10:42:41 +0700

    update pageAdmin thêm sản phẩm

--- src/app/controllers/product.php
+++ src/app/controllers/product.php
@@ -20,4 +20,31 @@ class product extends DController
         $this->load->view('cpanel/product/add_product');
         $this->load->view('cpanel/footer');
     }
+    public function insert_product()
+    {
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
+        $image = $_POST['image'];
+        $data = array(
+            'Name' => $name,
+            'Category' => $category,
+            'Description' => $description,
+            'Price' => $price,
+            'Image' => $image
+        );
+        $categorymodel = $this->load->model('categorymodel');
+        $table = "product";
+        $result = $categorymodel->insertItem($table, $data);
+        if ($result != 0) {
+            $message['msg'] = 'Thêm sản phẩm thành công.';
+            header('Location:' . BASE_URL . '/product?msg=' . urlencode(serialize($message)));
+            exit();
+        } else {
+            $message['msg'] = 'Thêm sản phẩm thất bại.';
+            header('Location:' . BASE_URL . '/product?msg=' . urlencode(serialize($message)));
+            exit();
+        }
+    }
 }
--- src/app/views/cpanel/product/add_product.php
+++ src/app/views/cpanel/product/add_product.php
@@ -1,17 +1,36 @@
+<?php
+if (!empty($_GET['msg'])) {
+    $msg = unserialize(urldecode($_GET['msg']));
+    foreach ($msg as $value) {
+        echo $value;
+    }
+}
+?>
 <h3>Thêm danh mục sản phẩm</h3>
-<form>
+<form action="<?php echo BASE_URL ?>\product\insert_product" method="POST">
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Tên danh mục</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label class="form-label">Tên sản phẩm</label>
+        <input type="text" name="name" class="form-control">
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Miêu tả danh mục</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label class="form-label">Loại sản phẩm</label>
+        <input type="text" name="category" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Miêu tả sản phẩm </label>
+        <input type="text" name="description" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Gía sản phẩm</label>
+        <input type="int" name="price" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Hình sản phẩm</label>
+        <input type="text" name="image" class="form-control">
     </div>
     <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
-    <button type="submit" class="btn btn-primary">Thêm danh mục</button>
+    <button type="submit" class="btn btn-primary">Thêm</button>
 </form>
